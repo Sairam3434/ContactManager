ad4758909ccdf35d5df98920ebfaf3ef25138048
praka <praka@DESKTOP-0VA4C0V>
2024-09-21 03:44:17 +0530

    code

---
+++ src/main/java/com/prakash/SmartContactManager/SmartContactMannagerApplication.java
@@ -0,0 +1,13 @@
+package com.prakash.SmartContactManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContactMannagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContactMannagerApplication.class, args);
+	}
+
+}
